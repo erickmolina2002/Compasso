23b4048f3a1282dc3be376aa833574e3c42155f9
Erick Molina Barreiros <erick.barreiros@teknisa.com>
2024-09-14 17:59:24 -0300

    feat: first steps of project with permission, roles, users

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,34 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Policies\UserPolicy;
+use App\Policies\RolePolicy;
+use App\Policies\PermissionPolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    protected $policies = [
+        User::class => UserPolicy::class,
+        Role::class => RolePolicy::class,
+        Permission::class => PermissionPolicy::class,
+    ];
     public function register(): void
     {
-        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot()
     {
-        //
+        $this->registerPolicies();
+
+        Gate::before(function (User $user, $ability) {
+            if ($user->hasRole('Admin')) {
+                return true;
+            }
+        });
     }
 }
